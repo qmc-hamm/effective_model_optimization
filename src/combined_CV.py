cf8d929b85ba1e4dfd33f25a2b14d16760017d08
sonalij3 <sonalij3@illinois.edu>
2024-07-23 13:04:50 -0500

    combined_CV.py : flake8 + documentation
    - Abides by most pep8 standards
    - Added doctrings
    - Correct variable name: outfile _> infile

--- src/combined_CV.py
+++ src/combined_CV.py
@@ -1,48 +1,74 @@
 import h5py
-import numpy as np
-from rich import print
-import h5py
 import pandas as pd
-import matplotlib.pyplot as plt
 
-def unpack_data(outfile, parameters):
+
+def unpack_data(infile:str, parameters:list[str]):
+    """ Unpack Optimized Solved Model from *.hdf5 files. Exports the loss data, setting parameters, and model
+    parameters.
+
+    Parameters
+    ----------
+    infile : str
+        Optimized model file name (*.hdf5).
+    parameters : list[str]
+        List of model paramters.
+
+    Returns
+    -------
+    dict
+        dict to be turned into csv file.
+    """
     unpack_model = {}
-    with h5py.File(outfile, "r") as f:
-        unpack_model["para_w_1"] = f["para_w_1"][()]
+    with h5py.File(infile, "r") as f:
+        unpack_model["w0"] = f["para_w_0"][()]
         unpack_model["loss_loss"] = f["loss_loss"][()]
 
         unpack_model["nstates_train"] = f["nstates_train"][()]
         unpack_model["nstates_test"] = f["nstates_test"][()]
         unpack_model["state_ind_for_test"] = f["state_ind_for_test"][()]
 
-        unpack_model["train/loss"] = f["train/"+"loss"][()] / unpack_model["nstates_train"]
-        unpack_model["train/sloss"] = f["train/"+"sloss"][()] / unpack_model["nstates_train"]
-        unpack_model["train/dloss"] = f["train/"+"dloss"][()] / unpack_model["nstates_train"]
-        unpack_model["train/penalty"] = f["train/"+"penalty"][()] / unpack_model["nstates_train"]
+        unpack_model["train/loss"] = f["train/" + "loss"][()] / unpack_model["nstates_train"]
+        unpack_model["train/sloss"] = f["train/" + "sloss"][()] / unpack_model["nstates_train"]
+        unpack_model["train/dloss"] = f["train/" + "dloss"][()] / unpack_model["nstates_train"]
+        unpack_model["train/penalty"] = f["train/" + "penalty"][()] / unpack_model["nstates_train"]
 
-        unpack_model["test/loss"] = f["test/"+"loss"][()] / unpack_model["nstates_test"]
-        unpack_model["test/sloss"] = f["test/"+"sloss"][()] / unpack_model["nstates_test"]
-        unpack_model["test/dloss"] = f["test/"+"dloss"][()] / unpack_model["nstates_test"]
-        unpack_model["test/penalty"] = f["test/"+"penalty"][()] / unpack_model["nstates_test"]
+        unpack_model["test/loss"] = f["test/" + "loss"][()] / unpack_model["nstates_test"]
+        unpack_model["test/sloss"] = f["test/" + "sloss"][()] / unpack_model["nstates_test"]
+        unpack_model["test/dloss"] = f["test/" + "dloss"][()] / unpack_model["nstates_test"]
+        unpack_model["test/penalty"] = f["test/" + "penalty"][()] / unpack_model["nstates_test"]
 
         for i, kk in enumerate(parameters):
-            unpack_model["rdmd_params/" + kk] = f["train/"+"rdmd_params/" + kk][()]
+            unpack_model["rdmd_params/" + kk] = f["train/" + "rdmd_params/" + kk][()]
 
     return unpack_model
 
 
-def make_avg_CSV_file(nCV:int, model_name:str, parameters:list, dir:str):
+def make_avg_CSV_file(nCV:int, model_name:str, parameters:list[str], dir:str):
+    """_summary_
 
-    unpacked_model_files = [] 
+    Parameters
+    ----------
+    nCV : int
+        Number of CV runs.
+    model_name : str
+        Name of model in directory structure.
+    parameters : list[str]
+        List of model paramters.
+    dir : str
+        Name of director header.
+    """
+
+    unpacked_model_files = []
 
     for i in range(nCV):
-        outfile = f'{dir}/{model_name}/CV{i}_model_output.hdf5'
-        unpacked_model_files.append(unpack_data(outfile, parameters))
+        infile = f'{dir}/{model_name}/CV{i}_model_output.hdf5'
+        unpacked_model_files.append(unpack_data(infile, parameters))
 
     df = pd.DataFrame(unpacked_model_files)
-    df.to_csv(f"{dir}/{model_name}_nCV{nCV}.csv", index_label='run') 
+    df.to_csv(f"{dir}/{model_name}_nCV{nCV}.csv", index_label='run')
 
-    return 
+    return
+
 
 if __name__ == "__main__":
     nCV = 30

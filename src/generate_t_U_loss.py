8d2bbc63a868621c662b1fb21681ec50bd931882
sonalij3 <sonalij3@illinois.edu>
2024-07-23 15:21:04 -0500

    generate_t_U_loss.py : pep8 + docstring
    - Abides by most pep8 rules except max-line-length
    - Fixed and added docstrings

--- src/generate_t_U_loss.py
+++ src/generate_t_U_loss.py
@@ -1,8 +1,8 @@
 import h5py
 import pandas as pd
 import loss_function
-import os
 import numpy as np
+
 
 def return_set_up_h4(
     named_terms,
@@ -15,12 +15,12 @@ def return_set_up_h4(
     w_0=1,
     beta=0,
 ):
-    """_summary_
+    """Set up H4 molecule as an example.
 
     Parameters
     ----------
-    named_terms : _type_
-        _description_
+    named_terms : str
+        File name for symmetry invariant terms.
     ai_descriptors : pd.DataFrame
         The descriptor information of the ab initio data. The keys should match model descriptors'. ab initio data, columns are parameter names, and 'energy'
     model_descriptors : str
@@ -53,7 +53,7 @@ def return_set_up_h4(
             twobody_keys.append(k)
     ai_df = pd.read_csv(ai_descriptors)
     ai_df = ai_df[ai_df.E0 > minimum_1s_occupation]
-    ai_df = ai_df[ ai_df.U < 1.3]  # Need to remove the top two states from the optimization
+    ai_df = ai_df[ai_df.U < 1.3]  # Need to remove the top two states from the optimization
 
     matches = onebody_params + twobody_params
 
@@ -63,54 +63,61 @@ def return_set_up_h4(
 
     return ai_df, beta, nroots, weights, onebody, twobody, matches
 
-def generate_t_U(tmin:float, tmax:float, Umin:float, Umax:float, eps:float=-0.5, ngrid:int=10, fname:str="loss_grid.csv"):
+
+def generate_t_U(tmin: float,
+                 tmax: float,
+                 Umin: float,
+                 Umax: float,
+                 eps: float = -0.5,
+                 ngrid: int = 10,
+                 outfile: str = "loss_grid.csv"):
     """_summary_
 
     Parameters
     ----------
     tmin : float
-        _description_
+        Starting t range. t is the hopping parameter.
     tmax : float
-        _description_
+        Ending t range.
     Umin : float
-        _description_
+        Starting U range. U is the double occupancy parameter.
     Umax : float
-        _description_
+        Ending U range.
     eps : float, optional
-        _description_, by default -0.5
+        E0 value or site/orbital energy., by default -0.5
     ngrid : int, optional
-        _description_, by default 10
-    fname : str, optional
-        _description_, by default "loss_grid.csv"
+        How many grid points in 1D, by default 10
+    outfile : str, optional
+        File name to save to, by default "loss_grid.csv"
     """
 
     onebody_params = ["E0", "t"]
     twobody_params = ["U"]  
-    keys = onebody_params+twobody_params
+    keys = onebody_params + twobody_params
 
-    ai_df, beta, nroots, weights, onebody, twobody, matches = return_set_up_h4(
-                                "../h4_data/named_terms_new.hdf5",
-                                "../h4_data/ai_descriptors.csv",
-                                "model_output.hdf5",
-                                nroots=36, # (4 sites choose 2 electrons)^2 , ^2 is for both spin up and spin down
-                                onebody_params=onebody_params,
-                                twobody_params=twobody_params,
-                                minimum_1s_occupation=3.7,
-                                w_0=0.8,
-                                beta=0.0,
-                            )
+    set_up = return_set_up_h4("../h4_data/named_terms_new.hdf5",
+                              "../h4_data/ai_descriptors.csv",
+                              "model_output.hdf5",
+                              nroots=36,  # (4 sites choose 2 electrons)^2 , ^2 is for both spin up and spin down
+                              onebody_params=onebody_params,
+                              twobody_params=twobody_params,
+                              minimum_1s_occupation=3.7,
+                              w_0=0.8,
+                              beta=0.0,
+                              )
+
+    ai_df, beta, nroots, weights, onebody, twobody, matches = set_up
 
     norm = 2 * np.var(ai_df)
     max_ai_energy = max(ai_df["energy"])
     boltzmann_weights = loss_function.give_boltzmann_weights(ai_df["energy"], ai_df["energy"][0], beta)
 
-    # for loops through t and U variables
     loss_grid = []
     for t in np.linspace(tmin, tmax, num=ngrid):
         print(f"t {t}")
         for U in np.linspace(Umin, Umax, num=ngrid):
 
-            params = [eps, t, U] # loop through these with same ordering as keys
+            params = [eps, t, U]  # loop through these with same ordering as keys
             data = loss_function.evaluate_loss(
                 params,
                 keys,
@@ -131,21 +138,20 @@ def generate_t_U(tmin:float, tmax:float, Umin:float, Umax:float, eps:float=-0.5,
             new_data["sloss"] = data["sloss"]
             new_data["dloss"] = data["dloss"]
             new_data["penalty"] = data["penalty"]
-            #new_data["para_w_0"] = data["para_w_0"] 
+
             for i, k in enumerate(keys):
                 new_data[k] = data["params"][i]
 
             loss_grid.append(new_data)
 
     df = pd.DataFrame(loss_grid)
-    df.to_csv(fname) 
+    df.to_csv(outfile)
 
     return
 
+
 if __name__ == "__main__":
     ngrid = 50
-    #eps = -0.5
-    #generate_t_U(0.0, -0.05, 0.1, 0.4, eps=eps, ngrid=ngrid, fname=f"loss_grid{ngrid}_eps{eps}.csv")
 
     eps = -0.498
-    generate_t_U(0.0, -0.05, 0.1, 0.4, eps=eps, ngrid=ngrid, fname=f"loss_grid{ngrid}_eps{eps}.csv")
+    generate_t_U(0.0, -0.05, 0.1, 0.4, eps=eps, ngrid=ngrid, outfile=f"loss_grid{ngrid}_eps{eps}.csv")

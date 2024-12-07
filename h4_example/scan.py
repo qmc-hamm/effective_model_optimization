0ef021835835c72b50e79b75e7c849e1ea4c6b9b
Ben Galewsky <ben@peartreestudio.net>
2024-12-07 15:44:06 -0600

    Make scan able to run local test or use slurm and change settings accordingly

--- h4_example/scan.py
+++ h4_example/scan.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import mlflow
@@ -5,20 +6,31 @@ from mlflow import MlflowClient
 from mlflow.entities import Param, RunTag
 
 
-def run_train(experiment_id, iters, backend_config="cpu-slurm.json", parent_run_id=None):
-    p = mlflow.projects.run(
-        uri=os.path.dirname(os.path.realpath(__file__)),
-        entry_point="main",
-        parameters={
+def run_train(experiment_id,
+              iters,
+              training_backend,
+              training_backend_config,
+              parent_run_id=None):
+    print(f"Starting run with backend {training_backend} and config {training_backend_config}")
+
+    kwargs = {
+        "uri": os.path.dirname(os.path.realpath(__file__)),
+        "entry_point": "main",
+        "parameters": {
             "niter_opt": str(iters),
             "tol_opt": str(iters),
             "nCV_iter": str(iters)
         },
-        experiment_id=experiment_id,
-        synchronous=False,
-        backend="slurm",
-        backend_config=backend_config
-    )
+        "experiment_id": experiment_id,
+        "synchronous": True if training_backend == "local" else False,
+        "backend": training_backend,
+    }
+
+    if training_backend != "local":
+        kwargs["backend_config"] = training_backend_config
+
+    p = mlflow.projects.run(**kwargs)
+
     MlflowClient().log_batch(run_id=p.run_id, metrics=[],
                              params=[Param("niter_opt", str(iters)),
                                      Param("tol_opt", str(iters)),
@@ -27,6 +39,14 @@ def run_train(experiment_id, iters, backend_config="cpu-slurm.json", parent_run_
 
     return p
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--training_backend", type=str)
+parser.add_argument("--training_backend_config",
+                    type=str,
+                    required=False,
+                    default=None)
+
+args = parser.parse_args()
 provided_run_id = os.environ.get("MLFLOW_RUN_ID", None)
 with mlflow.start_run(run_id=provided_run_id) as run:
     print("Search is run_id ", run.info.run_id)
@@ -37,6 +57,8 @@ with mlflow.start_run(run_id=provided_run_id) as run:
         jobs.append(run_train(
             experiment_id,
             iters=iters*100,
+            training_backend=args.training_backend,
+            training_backend_config=args.training_backend_config,
             parent_run_id=provided_run_id)
         )
         results = map(lambda job: job.wait(), jobs)

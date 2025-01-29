2863985794b5dc37f693cec176f4228902eeec85
sonalij3 <sonalij3@illinois.edu>
2025-01-28 21:40:51 -0600

    Initial commit of h-chain model solver

---
+++ h-chain_example/cross_validation_function.py
@@ -0,0 +1,148 @@
+import tempfile
+from tempfile import tempdir
+
+import h5py
+import pandas as pd
+import loss_function_function as loss_function
+import os
+import itertools
+import argparse
+import sys
+import mlflow
+
+from plot_model import plot_model
+
+all_rs = [2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.6, 4.0, 4.4, 5.0] # all ai_data rs
+
+def runCV(named_terms,
+          ai_dir,
+          model_descriptors,
+          nroots,
+          onebody_params,
+          twobody_params,
+          train_rs,
+          test_rs,
+          param_functions,
+          minimum_1s_occupation=3.7,
+          w0=1,
+          beta=0,
+          p=1,
+          guess_params=None,
+          state_cutoff=None,
+          niter_opt=2,
+          tol_opt=1e-2,
+          maxfev_opt=1000.0,
+          tmpdirname=None
+):
+
+    onebody = {}
+    twobody = {}
+    onebody_keys = []
+    twobody_keys = []
+    with h5py.File(named_terms, "r") as f:
+        for k in f["onebody"].keys():
+            onebody[k] = f[f"onebody/{k}"][()]
+            onebody_keys.append(k)
+        for k in f["twobody"].keys():
+            twobody[k] = f[f"twobody/{k}"][:]
+            twobody_keys.append(k)
+
+    ai_df_rs = {}
+    for r in all_rs:
+        ai_df = pd.read_csv(ai_dir)
+        ai_df = ai_df[ai_df.r == r]
+        ai_df = ai_df[ai_df.doccp < 0.8]  # Only doing spin states for now
+        if state_cutoff is not None:
+            ai_df = ai_df[ai_df.state < state_cutoff]
+        ai_df = ai_df.reset_index()
+        ai_df_rs[f'r{r}'] = ai_df
+
+    #print(ai_df_rs)
+
+    matches = onebody_params + twobody_params
+    weights = [w0, 1 - w0]
+
+    loss_function.mapping(
+        onebody,
+        twobody,
+        onebody_params,
+        twobody_params,
+        ai_df_rs,
+        nroots,
+        model_descriptors,
+        matches,
+        train_rs,
+        test_rs,
+        param_functions,
+        weights,
+        beta,
+        p,
+        guess_params,
+        niter_opt = niter_opt,
+        tol_opt = tol_opt,
+        maxfev_opt = maxfev_opt,
+    )
+
+
+def make_name(parameters):
+    return "_".join(parameters[0]) + "_" + "_".join(parameters[1])
+
+def main(parameters, state_cutoff, w0, train_rs, niter_opt, tol_opt, maxfev_opt, nCV_iter, param_functions):
+        test_rs = list( set(all_rs) - set(train_rs))#.sort()
+        test_rs.sort()
+        print("Test rs values:", test_rs)
+        with mlflow.start_run():
+            # Write model and plots to temp dir
+            with tempfile.TemporaryDirectory() as output_dir:
+                model_files = []
+                for i in range(nCV_iter):
+                    pname = make_name(parameters)
+                    dirname = os.path.join(output_dir,f"func_model_data_{state_cutoff}_{w0}")
+                    if not os.path.exists(dirname):
+                        os.makedirs(dirname)
+                    model_file_path = f"{dirname}/{pname}_{i}.hdf5"
+
+                    runCV(named_terms="hchain4_named_operators.hdf5",
+                          ai_dir="ai_data/hchain4.csv",
+                          model_descriptors=model_file_path,
+                          nroots=36,
+                          onebody_params=parameters[0],
+                          twobody_params=parameters[1],
+                          train_rs=train_rs,
+                          test_rs=test_rs,
+                          param_functions=param_functions,
+                          w0=w0,
+                          beta=0,
+                          p=1,
+                          state_cutoff=state_cutoff,
+                          niter_opt=niter_opt,
+                          tol_opt=tol_opt,
+                          maxfev_opt=maxfev_opt
+                          )
+                    model_files.append(model_file_path)
+                plot_model(output_dir, model_files, parameters) # Artifacts the plots to mlflow inside function
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parameters", type=str, nargs="+")
+    parser.add_argument("--train_rs", type=str)
+    parser.add_argument("--state_cutoff", type=int)
+    parser.add_argument("--w0", type=float)
+    parser.add_argument("--niter_opt", type=int)
+    parser.add_argument("--tol_opt", type=float)
+    parser.add_argument("--nCV_iter", type=int, default=1)
+    parser.add_argument("--maxfev_opt", type=int, default=1000)
+    parser.add_argument("--parameter_functions", type=str, nargs="+")
+    # param_functions = ["func_E0", "func_t", "func_U"]
+    args = parser.parse_args()
+    parameters = (args.parameters[0].split(','), args.parameters[1].split(','))
+    state_cutoff = args.state_cutoff
+    w0 = args.w0
+    train_rs = [float(r) for r in args.train_rs.split(",")]
+    niter_opt = args.niter_opt
+    tol_opt = args.tol_opt
+    nCV_iter = args.nCV_iter
+    maxfev_opt = args.maxfev_opt
+    param_functions = args.parameter_functions[0].split(',') + args.parameter_functions[1].split(',')
+
+    main(parameters, state_cutoff, w0, train_rs, niter_opt, tol_opt, maxfev_opt, nCV_iter, param_functions)
---
+++ h-chain_example/plot_model.py
@@ -0,0 +1,115 @@
+import tempfile
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import h5py
+import os
+import numpy as np
+
+import mlflow
+
+
+def make_name(parameters):
+    return "_".join(parameters[0]) + "_" + "_".join(parameters[1])
+
+
+def gather_data_to_plot(dirname, fnames, parameters):
+    data = []
+
+    train_rmse = []
+    val_rmse = []
+    test_rmse = []
+    loss = []
+
+    for i, fname in enumerate(fnames):
+        with h5py.File(os.path.join(dirname, fname), 'r') as f:
+            train_rs = f['train_rs'][()]
+            test_rs = f['test_rs'][()]
+
+            all_rs = list(train_rs)+list(test_rs)
+            all_rs.sort()
+
+            train_rmse.append( f["Mean over r Spectrum RMSE (Ha) - Train"][()] )
+            val_rmse.append( f["Mean over r Spectrum RMSE (Ha) - Validation"][()] )
+            test_rmse.append( f["Mean over r Spectrum RMSE (Ha) - Test"][()] )
+            loss.append( f["loss"][()] )
+
+            for r in all_rs:
+                data_r = {}
+                data_r['r (Bohr)'] = r
+
+                data_r['CV iteration'] = i
+
+                data_r['loss'] = f['loss'][()]
+
+                if r in train_rs:
+                    data_r['Spectrum RMSE Train (Ha)'] = f[f'r{r}/Spectrum RMSE Train (Ha)'][()]
+                    data_r['Spectrum RMSE Val (Ha)'] = f[f'r{r}/Spectrum RMSE Val (Ha)'][()]
+                    data_r['Spectrum RMSE Test (Ha)'] = None
+                elif r in test_rs:
+                    data_r['Spectrum RMSE Train (Ha)'] = None
+                    data_r['Spectrum RMSE Val (Ha)'] = None
+                    data_r['Spectrum RMSE Test (Ha)'] = f[f'r{r}/Spectrum RMSE (Ha)'][()]
+
+                for parameter in parameters:
+                    data_r[f'RDMD {parameter} (Ha)'] = f[f'r{r}/rdmd_params/{parameter}'][()]
+                    data_r[f'DMD {parameter} (Ha)'] = f[f'r{r}/dmd_params/{parameter}'][()]
+
+                data.append(data_r)
+
+    mlflow.log_metrics({
+    "CV-avg_mean-over-r_spectrum-rmse_ha_train": np.mean(train_rmse),
+    "CV-avg_mean-over-r_spectrum-rmse_ha_val": np.mean(val_rmse),
+    "CV-avg_mean-over-r_spectrum-rmse_ha_test": np.mean(test_rmse),
+    "CV-avg_loss": np.mean(loss),
+    })
+
+    return pd.DataFrame(data)
+
+
+def plot_model(dirname: str, fnames: list[str], parameters: tuple[list[str], list[str]]) -> str:
+    df = gather_data_to_plot(dirname, fnames, parameters[0]+parameters[1])
+    
+    csv_file = f'{dirname}/Processed_CV_data.csv'
+    df.to_csv(csv_file)
+    mlflow.log_artifact(csv_file)
+
+    plot_file = f'{dirname}/Spectrum_RMSEvs_r.png'
+    sns.lineplot(data=df, x='r (Bohr)', y='Spectrum RMSE Train (Ha)', label="Train")
+    sns.lineplot(data=df, x='r (Bohr)', y='Spectrum RMSE Val (Ha)', label="Validation")
+    sns.lineplot(data=df, x='r (Bohr)', y='Spectrum RMSE Test (Ha)', label="Test")
+    plt.ylabel("Spectrum RMSE (eV)")
+    plt.legend()
+    plt.savefig(plot_file, dpi=200)
+    plt.clf()
+
+    mlflow.log_artifact(plot_file)
+
+    for parameter in parameters[0]+parameters[1]:
+        plot_file2 = f'{dirname}/RDMD_{parameter}vs_r.png'
+        sns.lineplot(data=df, x='r (Bohr)', y=f'RDMD {parameter} (Ha)', label="RDMD")
+        sns.lineplot(data=df, x='r (Bohr)', y=f'DMD {parameter} (Ha)', label="DMD")
+        plt.savefig(plot_file2, dpi=200)
+        plt.clf()
+
+        mlflow.log_artifact(plot_file2)
+
+    return
+
+
+if __name__ == "__main__":
+
+    state_cutoff = 10
+    w0 = 0.9
+    parameters = (['E0', 't'], ['U'])
+    pname = make_name(parameters)
+    i = 0
+
+    files = [f"func_model_data_{state_cutoff}_{w0}/{pname}_{i}.hdf5"]
+
+    plot_model(".", files,
+               (['E0', 't'], ['U']) )
+
+    
+        

e2c65251721bd133d788b0c5703ae2f728464be3
sonalij3 <sonalij3@illinois.edu>
2025-02-05 00:18:20 -0600

    Add new inference testing

--- h-chain_example/cross_validation_function.py
+++ h-chain_example/cross_validation_function.py
@@ -79,6 +79,68 @@ def runCV(named_terms,
         maxfev_opt = maxfev_opt,
     )
 
+def runInference(named_terms,
+          ai_dir,
+          model_descriptors,
+          inference_name,
+          nroots,
+          onebody_params,
+          twobody_params,
+          rs,
+          minimum_1s_occupation=3.7,
+          state_cutoff=None,
+          tmpdirname=None
+):
+
+    onebody = {}
+    twobody = {}
+    onebody_keys = []
+    twobody_keys = []
+    with h5py.File(named_terms, "r") as f:
+        for k in f["onebody"].keys():
+            onebody[k] = f[f"onebody/{k}"][()]
+            onebody_keys.append(k)
+        for k in f["twobody"].keys():
+            twobody[k] = f[f"twobody/{k}"][:]
+            twobody_keys.append(k)
+
+    ai_df_rs = {}
+    for r in all_rs:
+        ai_df = pd.read_csv(ai_dir)
+        ai_df = ai_df[ai_df.r == r]
+        ai_df = ai_df[ai_df.doccp < 0.8]  # Only doing spin states for now
+        if state_cutoff is not None:
+            ai_df = ai_df[ai_df.state < state_cutoff]
+        ai_df = ai_df.reset_index()
+        ai_df_rs[f'r{r}'] = ai_df
+
+    #print(ai_df_rs)
+
+    matches = onebody_params + twobody_params
+
+    params = {}
+
+    with h5py.File(model_descriptors, 'r') as f:
+        for r in rs:
+            param_list = []
+            for parameter in matches:
+                param_list.append(f[f'r{r}/rdmd_params/{parameter}'][()])
+            params[f'r{r}'] = param_list
+
+    loss_function.inference(
+        onebody,
+        twobody,
+        onebody_params,
+        twobody_params,
+        ai_df_rs,
+        inference_name,
+        nroots,
+        model_descriptors,
+        matches,
+        rs,
+        params
+    )
+
 
 def make_name(parameters):
     return "_".join(parameters[0]) + "_" + "_".join(parameters[1])
@@ -110,8 +172,43 @@ def main(parameters, state_cutoff, w0, train_rs, niter_opt, tol_opt, maxfev_opt,
                           tol_opt=tol_opt,
                           maxfev_opt=maxfev_opt
                           )
+
+                    runInference(named_terms="hchain6_named_operators.hdf5",
+                          ai_dir="ai_data/hchain6.csv",
+                          model_descriptors=model_file_path,
+                          inference_name="natoms6_casscf",
+                          nroots=40,
+                          onebody_params=parameters[0],
+                          twobody_params=parameters[1],
+                          rs=train_rs,
+                          state_cutoff=state_cutoff,
+                          )
+
+                    runInference(named_terms="hchain8_named_operators.hdf5",
+                          ai_dir="ai_data/hchain8_casscf.csv",
+                          model_descriptors=model_file_path,
+                          inference_name="natoms8_casscf",
+                          nroots=100,
+                          onebody_params=parameters[0],
+                          twobody_params=parameters[1],
+                          rs=[3.0],#[2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.6, 4.0, 4.4],
+                          state_cutoff=state_cutoff,
+                          )
+
+                    runInference(named_terms="hchain8_named_operators.hdf5",
+                          ai_dir="ai_data/hchain8_vmc.csv",
+                          model_descriptors=model_file_path,
+                          inference_name="natoms8_vmc",
+                          nroots=100,
+                          onebody_params=parameters[0],
+                          twobody_params=parameters[1],
+                          rs=[3.0],#[2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.6, 4.0, 4.4],
+                          state_cutoff=state_cutoff,
+                          ) 
+
+                    mlflow.log_artifact(model_file_path)
                     model_files.append(model_file_path)
-                plot_model(output_dir, model_files, parameters) # Artifacts the plots to mlflow inside function
+                plot_model(output_dir, model_files, ["natoms6_casscf", "natoms8_casscf", "natoms8_vmc"], parameters) # Artifacts the plots to mlflow inside function
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

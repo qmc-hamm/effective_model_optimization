42dae38b4e825465637086a4c465d0db99bad780
sonalij3 <sonalij3@illinois.edu>
2025-02-05 00:14:04 -0600

    Remove old boltzmann weighting

--- h-chain_example/loss_function_function.py
+++ h-chain_example/loss_function_function.py
@@ -220,31 +220,6 @@ def unmapped_penalty(energy_m_unmapped, max_energy_ab: float, norm):
     return penalty
 
 
-def give_boltzmann_weights(energy_ab, e_0, beta):
-    """Return the boltzmann weights given a beta
-
-    Parameters
-    ----------
-    energy_ab : nd.array(float)
-        Length of ab initio states, the energy of each state.
-    e_0 : float
-        The ground state energy
-    beta : float
-        Boltmann beta, sets the expentional decay of the state weighting.
-
-    Returns
-    -------
-    nd.array(float)
-        boltztman weights, the ground state always get 1. lower energy states get more weighting then exponentially
-        decays.
-    """
-    # give boltzmann weights given the boltzmann factor
-    # beta = 1/(k_b * T)
-
-    boltzmann_weights = np.exp(-beta * (energy_ab - e_0))
-    return boltzmann_weights
-
-
 def evaluate_loss(
     params: np.ndarray,
     keys,
@@ -606,7 +581,6 @@ def mapping(
     test_rs: list,
     param_functions: list,
     weights: list,
-    beta: float,
     p: int,
     guess_params=None,
     clip_val=1,
